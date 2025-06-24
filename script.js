a71b2af72fdb3d62051ee632a353df46a13007c2
Rehoboth Ajose <ajoserehoboth049@gmail.com>
2025-06-24 05:36:04 +0100

    Update script.js for the sermon animation

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
         const notif = document.querySelector('.card');
         notif.classList.toggle('active');
     }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+            console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show')
+            }
+            else{
+                entry.target.classList.remove('show')
+            }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
